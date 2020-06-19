63d26256e01577d60e31acc1230ba37718ec0a87
TogoFire <italomellopereira@gmail.com>
2020-06-19 10:12:14 -0300

    daisy: Add blobs Sony and Dolby
    * SonyMusic - By Manish4586 @xda
    * Dolby - https://forum.xda-developers.com/android/software/mm-p-dolby-digital-plus-arise-20181115-t3868192
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- daisy/daisy-vendor.mk
+++ daisy/daisy-vendor.mk
@@ -1914,7 +1914,17 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/daisy/proprietary/vendor/lib/libmmcamera_ov12a10_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_ov12a10_eeprom.so \
     vendor/xiaomi/daisy/proprietary/vendor/lib/libstagefright_soft_ac4dec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_soft_ac4dec.so \
     vendor/xiaomi/daisy/proprietary/vendor/lib/libstagefright_soft_ddpdec.so:$(TARGET_COPY_OUT_VENDOR)/vendor/lib/libstagefright_soft_ddpdec.so \
-    vendor/xiaomi/daisy/proprietary/vendor/lib/soundfx/libdseffect.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdseffect.so
+    vendor/xiaomi/daisy/proprietary/vendor/lib/soundfx/libdseffect.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdseffect.so \
+    vendor/xiaomi/daisy/proprietary/etc/permissions/com.sony.device.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.sony.device.xml \
+    vendor/xiaomi/daisy/proprietary/etc/permissions/com.sonymobile.mediacontent.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.sonymobile.mediacontent.xml \
+    vendor/xiaomi/daisy/proprietary/etc/permissions/com.sonyericsson.system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.sonyericsson.system.xml \
+    vendor/xiaomi/daisy/proprietary/framework/com.sony.device.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/com.sony.device.jar \
+    vendor/xiaomi/daisy/proprietary/framework/com.sonyericsson.system.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/com.sonyericsson.system.jar \
+    vendor/xiaomi/daisy/proprietary/framework/com.sonyericsson.music.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/com.sonyericsson.music.jar \
+    vendor/xiaomi/daisy/proprietary/framework/com.sonymobile.mediacontent.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/com.sonymobile.mediacontent.jar \
+    vendor/xiaomi/daisy/proprietary/etc/ds1-default.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/ds1-default.xml \
+    vendor/xiaomi/daisy/proprietary/etc/permissions/android.dolby.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.dolby.xml \
+    vendor/xiaomi/daisy/proprietary/framework/dolby_ds.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/dolby_ds.jar
 
 
 PRODUCT_PACKAGES += \
